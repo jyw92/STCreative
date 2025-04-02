0bc5e167dd6292e62428b56d0a174daecb533b53
jyw92 <koreajyy@gmail.com>
2025-04-03 01:46:56 +0900

    test

--- resource/js/page/home.js
+++ resource/js/page/home.js
@@ -2,11 +2,11 @@ function home() {
   console.log("home");
   homeParallax();
   spotlightAnimation();
+  heroAnimation();
   // const DOM = {
   //   area: document.querySelector('.section01'),
   //   video: document.querySelector('video'),
   // }
-
 
   // const fixedHeader = gsap.timeline()
   // .to('#header', { backgroundColor:'transparent'})
@@ -14,62 +14,113 @@ function home() {
   // .to('#logo img', {attr:{src:'./resource/images/logo_STC.svg'}},0)
 }
 
-
 function spotlightAnimation() {
   // const mainVis = document.querySelector('.no-main-vis');
 
   // if (!mainVis) return;
   gsap.fromTo(
-      '.section01--spotlight div',
-      {
-          x: '-75%',
-      },
-      {
-          x: '75%',
-          repeat: -1,
-          yoyo: true,
-          duration: 2.5,
-          ease: 'none',
-      }
+    ".section01--spotlight div",
+    {
+      x: "-75%",
+    },
+    {
+      x: "75%",
+      repeat: -1,
+      yoyo: true,
+      duration: 2.5,
+      ease: "none",
+    }
   );
 }
 
-function heroAnimation(){
+function heroAnimation() {
   const DOM = {
-    // area: document.querySelector('.')
-  }
+    area: document.querySelector(".section01--main"),
+    video: document.querySelector(".section01--main--video"),
+  };
+  ScrollTrigger.create({
+    id: "section01--main",
+    trigger: DOM.area,
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    pinSpacing: false,
+    invalidateOnRefresh: true,
+    anticipatePin: 1,
+  });
+  let tl;
+  ScrollTrigger.matchMedia({
+    "(min-width: 769px)": function () {
+      tl = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: DOM.area,
+            start: "20% bottom",
+            end: "100% bottom",
+            scrub: 1,
+            id: "main-ani",
+            // markers: true,
+          },
+        })
+        .to(DOM.video, {
+          clipPath: "circle(10% at 50% 50%)",
+          duration: 1,
+        })
+        .to(DOM.video, {
+          clipPath: "circle(100% at 50% 50%)",
+          duration: 1.5,
+        });
+    },
+    "(max-width: 768px)": function () {
+      tl = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: DOM.area,
+            start: "20% bottom",
+            end: "100% bottom",
+            scrub: 1,
+            id: "main-ani",
+            // markers: true,
+          },
+        })
+        .to(DOM.video, {
+          clipPath: "circle(20% at 50% 50%)",
+          duration: 1,
+        })
+        .to(DOM.video, {
+          clipPath: "circle(100% at 50% 50%)",
+          duration: 1.5,
+        });
+    },
+  });
 }
 
-
-
-function homeParallax(){
-
+function homeParallax() {
   const parallax = gsap.timeline({
-    scrollTrigger:{
-      trigger:'.section02',
-      start:'top top',
-      end:'+=300%',
-      markers:true,
-      scrub:true,
-    }
-  })
+    scrollTrigger: {
+      trigger: ".section02",
+      start: "top top",
+      end: "+=300%",
+      markers: true,
+      scrub: true,
+    },
+  });
 
   const fixedVisualText = gsap.timeline({
-    scrollTrigger:{
-      trigger:'.section02--main--visual__text strong',
-      start:'top top',
-      end:'+=300%',
-      markers:true,
-      scrub:1,
+    scrollTrigger: {
+      trigger: ".section02--main--visual__text strong",
+      start: "top top",
+      end: "+=300%",
+      markers: true,
+      scrub: 1,
       pin: true,
-    }
-  })
-
-  gsap.utils.toArray('.parallax').forEach(layer => {
-      const depth = layer.dataset.depth;
-      const movement = -(layer.offsetHeight * depth);
-
-      parallax.to(layer, { y: movement, duration:2, ease: "none", }, 0)
+    },
   });
-  
+
+  gsap.utils.toArray(".parallax").forEach((layer) => {
+    const depth = layer.dataset.depth;
+    const movement = -(layer.offsetHeight * depth);
+
+    parallax.to(layer, { y: movement, duration: 2, scale:2, ease: "power3.inOut" }, 0);
+  });
 }
